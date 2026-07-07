58d02f208e770b1f8dd911ed7ca7adfe59b444ee
felipenascimentopa <felipejestison1010@gmail.com>
2026-07-07 17:16:53 -0300

    mudando nomes

--- backend/src/test/java/com/cefet/backend/BackendApplicationTests.java
+++ backend/src/test/java/com/cefet/backend/BackendApplicationTests.java
@@ -1,10 +1,10 @@
-package com.cefet.testroom;
+package com.cefet.backend;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class TestroomApplicationTests {
+class BackendApplicationTests {
 
 	@Test
 	void contextLoads() {
