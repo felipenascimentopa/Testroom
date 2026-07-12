d0af22519311cadae17b475652a1671adafeb41c
felipenascimentopa <felipejestison1010@gmail.com>
2026-07-12 14:48:00 -0300

    método de inserção de categoria na camada service

--- backend/src/main/java/com/cefet/backend/repository/CategoriaRepository.java
+++ backend/src/main/java/com/cefet/backend/repository/CategoriaRepository.java
@@ -3,7 +3,10 @@ package com.cefet.backend.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.cefet.backend.entity.Categoria;
+import com.cefet.backend.entity.Professor;
 
 public interface CategoriaRepository extends JpaRepository<Categoria, Long> {
 
+     public boolean existsByNomeAndCriador(String nome, Professor criador);
+     
 }
---
+++ backend/src/main/java/com/cefet/backend/service/CategoriaService.java
@@ -0,0 +1,45 @@
+package com.cefet.backend.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.cefet.backend.dto.CategoriaRequestDTO;
+import com.cefet.backend.dto.CategoriaResponseDTO;
+import com.cefet.backend.entity.Categoria;
+import com.cefet.backend.entity.Professor;
+import com.cefet.backend.exception.BusinessException;
+import com.cefet.backend.exception.ResourceNotFoundException;
+import com.cefet.backend.repository.CategoriaRepository;
+import com.cefet.backend.repository.ProfessorRepository;
+
+@Service
+public class CategoriaService {
+
+     @Autowired
+     private CategoriaRepository categoriaRepository;
+
+     @Autowired
+     private ProfessorRepository professorRepository;
+
+     @Transactional
+     public CategoriaResponseDTO inserir(CategoriaRequestDTO dto, Long professorId) {
+
+          Professor professor = professorRepository.findById(professorId)
+               .orElseThrow(() -> new ResourceNotFoundException("Professor não encontrado. Id: " + professorId));
+
+          if (categoriaRepository.existsByNomeAndCriador(dto.getNome(), professor)) {
+               throw new BusinessException("Já existe uma categoria com ete nome cadastrada pelo professor encontrado!");
+          }
+
+          Categoria categoria = new Categoria();
+          categoria.setNome(dto.getNome());
+          categoria.setDescricao(dto.getDescricao());
+          categoria.setCriador(professor);
+
+          return new CategoriaResponseDTO(categoriaRepository.save(categoria));
+     }
+
+     
+     
+}
