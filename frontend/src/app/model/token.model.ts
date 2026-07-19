3acf5dff49f5431c3e885995d62407b41b18a4a1
felipenascimentopa <felipejestison1010@gmail.com>
2026-07-19 14:59:15 -0300

    inicio token service no frontend

--- frontend/src/app/model/token.model.ts
+++ frontend/src/app/model/token.model.ts
@@ -1,2 +1,11 @@
 export class TokenModel {
+     id: string;
+     nome: string;
+     login: string;
+
+     constructor() {
+          this.id = "";
+          this.nome = "";
+          this.login = "";
+     }
 }
--- frontend/src/app/model/usuario.model.ts
+++ frontend/src/app/model/usuario.model.ts
@@ -1,2 +1,7 @@
 export class UsuarioModel {
+     login: string;
+
+     constructor() {
+          this.login = "";
+     }
 }
