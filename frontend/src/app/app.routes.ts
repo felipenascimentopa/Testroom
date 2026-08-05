40960be013cd6977b837c08e65604caf14a4f017
arthurx4k <arthuroliver423@gmail.com>
2026-08-05 15:19:21 -0300

    alterações

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -9,5 +9,17 @@ export const routes: Routes = [
   {
     path: 'login',
     loadComponent: () => import('./pages/login/login.page').then( m => m.LoginPage)
+  },  {
+    path: 'menu',
+    loadComponent: () => import('./pages/menu/menu.page').then( m => m.MenuPage)
   },
+  {
+    path: 'categoria',
+    loadComponent: () => import('./pages/categoria/categoria.page').then( m => m.CategoriaPage)
+  },
+  {
+    path: 'cadastro',
+    loadComponent: () => import('./pages/cadastro/cadastro.page').then( m => m.CadastroPage)
+  },
+
 ];
