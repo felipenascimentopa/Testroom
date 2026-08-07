82605fa032545428473dbd1f39d7de994da1b090
arthurx4k <arthuroliver423@gmail.com>
2026-08-06 22:12:49 -0300

    .

--- backend/src/main/java/com/cefet/backend/config/CorsConfig.java
+++ backend/src/main/java/com/cefet/backend/config/CorsConfig.java
@@ -9,7 +9,11 @@ public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:8100")
+                .allowedOrigins("http://localhost:8100",
+                        "https://api-testroom.onrender.com",
+                        "capacitor://localhost",
+                        "http://localhost",
+                        "file://*")
                 .allowedMethods("*")
                 .allowedHeaders("*");
     }
